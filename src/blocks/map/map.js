fe1d1f2cedff47deb502a186642ccc2de880c400
_tango <shurinskiy@mail.ru>
2023-07-07 21:45:49 +0600

    before adaptive

--- src/blocks/map/map.js
+++ src/blocks/map/map.js
@@ -4,15 +4,30 @@ import ymaps from 'ymaps';
 	ymaps.load('https://api-maps.yandex.ru/2.1/?lang=ru_RU').then(maps => {
 
 		const map = new maps.Map('map', {
-			center: [57.555691, 61.731427],
-			zoom: 12,
+			center: [12.9165, 100.8667],
+			zoom: 16,
 			controls: ['zoomControl'],
 		},{
 			zoomControlPosition: { right: '5%', top: 100 },
 			zoomControlFloat: 'none'
+		}),
+
+		goldfish = new maps.GeoObject({
+			geometry: {
+				type: "Point",
+				coordinates: [12.9165, 100.8667]
+			},
+			properties: {
+				hintContent: 'Gold Fish',
+				balloonContent: 'Мы оказываем <strong>консалтинговые услуги<br> в Таиланде</strong> для туристов, экспатов и бизнеса.<br>адрес: 352, Phratamnak 3, Pattaya City',
+			}
+		}, {
+			preset: 'islands',
+			iconColor: '#32BFC6'
 		});
 
 		map.behaviors.disable('scrollZoom');
+		map.geoObjects.add(goldfish);
 
 	})
 	.catch(error => console.log('Failed to load Yandex Maps', error));
